05c3d23a6c482324dfd3a791b18f13450c06a3ea
Igor Bezmestin <igor-rock@list.ru>
2020-02-09 17:12:10 +0300

    Поправлен прелоадер при запросе данных о карте пользователя на странице карты

--- src/containers/map/formRoute/formWarning/index.jsx
+++ src/containers/map/formRoute/formWarning/index.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { useSelector } from 'react-redux';
+import { getIsLoading } from "../../../../store/card";
+import Preloader from "../../../../shared/preloader";
 
 const FormWarning = () => {
-    return (
+    const isLoading = useSelector(state=> getIsLoading(state));
+
+    const preloaderTemplate = (
+        <div className="isLoading formRoute warning">
+            <Preloader isLoading={isLoading}/>
+        </div>
+    );
+
+    const warningTemplate = (
         <>
             <h2 className="form__title">Внимание</h2>
             <p className="form__description">Для работы с сервисом введите данные платежной карты в разделе "Профиль"</p>
         </>
     );
+
+    return (
+        <>
+            {isLoading ? preloaderTemplate : warningTemplate}
+        </>
+    );
+    
 }
 
 export default FormWarning;
